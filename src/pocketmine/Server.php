2e9e4cfe9d016e14b2fd7e7420c318f6ce624bcd
Ip <jun040329@gmail.com>
2017-09-16 11:30:54 +0900

    Update Server.php

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1599,14 +1599,8 @@ class Server
 			$this->forceLanguage = $this->getProperty("settings.force-language", false);
 			$this->baseLang = new BaseLang($this->getProperty("settings.language", BaseLang::FALLBACK_LANGUAGE));
 			//$this->logger->info($this->getLanguage()->translateString("language.selected", [$this->getLanguage()->getName(), $this->getLanguage()->getLang()]));
-
 			$this->memoryManager = new MemoryManager($this);
-			 $startupmsg = "\n       §8##§7\§b    _                           _ §r\n      §8##§7 /§b   | | _____   _____ _ __ _   _| |§r\n     §8##§7 /§b    | |/ _ \ \ / / _ \ '__| | | | |§r\n    §8##§7 /§b     | |  __/\ V /  __/ |  | |_| | |§r\n   §8##§7 /§b      |_|\___| \_/ \___|_|   \__, |_|§r\n  §8##§7 /§b                              |___/   §r\n §8#########§7\ §bGitHub.com/LeverylTeam/Leveryl  §r\n §7\________/§r";
-  			$lang = $this->getProperty("settings.language", BaseLang::FALLBACK_LANGUAGE);
-  			if(!file_exists($this->dataPath . "leveryl.yml")) {
-  				if(file_exists($this->filePath . "src/pocketmine/resources/leveryl_$lang.yml")) {
-		
-		");
+			$startupmsg = "\n       §8##§7\§b    _                           _ §r\n      §8##§7 /§b   | | _____   _____ _ __ _   _| |§r\n     §8##§7 /§b    | |/ _ \ \ / / _ \ '__| | | | |§r\n    §8##§7 /§b     | |  __/\ V /  __/ |  | |_| | |§r\n   §8##§7 /§b      |_|\___| \_/ \___|_|   \__, |_|§r\n  §8##§7 /§b                              |___/   §r\n §8#########§7\ §bGitHub.com/LeverylTeam/Leveryl  §r\n §7\________/§r";
 			$lang = $this->getProperty("settings.language", BaseLang::FALLBACK_LANGUAGE);
 			if(!file_exists($this->dataPath . "leveryl.yml")) {
 				if(file_exists($this->filePath . "src/pocketmine/resources/leveryl_$lang.yml")) {
@@ -1617,6 +1611,7 @@ class Server
 				@file_put_contents($this->dataPath . "leveryl.yml", $content);
 			}
 			$this->leverylconfig = new Config($this->dataPath . "leveryl.yml", Config::YAML, []);
+			$this->alwaysday = $this->leverylconfig->get("AlwaysDay");
 
 			$this->alwaysday = $this->leverylconfig->get("AlwaysDay");
 			$this->blockbreakparticles = $this->getLeverylConfigValue("BlockBreakParticles", true);
